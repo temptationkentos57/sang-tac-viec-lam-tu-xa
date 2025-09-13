2127707cc4de6ce086134a65501e9b92fd98d30e
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-13 17:11:22 +0200

    Refactor greeting message for clarity.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Remote Job Creation!');
+    res.send('Welcome to the Remote Job Creation API!');
 });
 
 app.listen(PORT, () => {
